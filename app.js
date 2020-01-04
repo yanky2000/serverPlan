cc506c0f95b3b874cd99bfdb301d3952050feb6a
Victor Yang <yanky2000@yahoo.com>
2020-01-04 14:45:38 +0300

    add uuid

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const RESPONES = require('./fixtures');
 const visits = require('./tempData');
-// import { visits } from './visits';
-
+const uuid = require('uuid/v1')
 const app = express();
 app.use(
 	cors({
@@ -24,45 +23,47 @@ app.get('/visits', (req, res) => {
 });
 app.post('/newvisit', (req, res) => {
 	console.log(req.body);
-	res.send(req.body);
+	const newId = uuid();
+	const newVisitWithId = {...req.body, visitId: newId}
+	res.send(newVisitWithId);
 });
 
-app.post('/parametersV2', (req, res) => {
-	res.set();
-	res.send(
-		RESPONES.paramsV2
-		// {
-		// corporate: { parameters: { key: "CHAT_ENABLE", value: "true" } },
-		// }
-	);
-});
-
-app.post('/fields', (req, res) => {
-	res.set();
-	res.send(RESPONES.fields);
-});
-app.post('/dictionary', (req, res) => {
-	res.set();
-	res.send(RESPONES.dictionary);
-});
-
-app.post('/history', (req, res) => {
-	res.send(RESPONES.history);
-});
-app.post('/registerV2', (req, res) => {
-	res.send(RESPONES.register);
-});
-app.post('/auth', (req, res) => {
-	res.send({ errorCode: -1 });
-});
-app.post('/sendPreview', (req, res) => {
-	res.send();
-});
-// app.post("/sessionsV2/read", (req, res) => {
-// res.send(RESPONES.read);
-// res.end()
-//     res.set("Connection", "close");
+// app.post('/parametersV2', (req, res) => {
+// 	res.set();
+// 	res.send(
+// 		RESPONES.paramsV2
+// 		// {
+// 		// corporate: { parameters: { key: "CHAT_ENABLE", value: "true" } },
+// 		// }
+// 	);
 // });
-app.post('/sessionsV2/subscribe', (req, res) => {
-	res.send();
-});
+
+// app.post('/fields', (req, res) => {
+// 	res.set();
+// 	res.send(RESPONES.fields);
+// });
+// app.post('/dictionary', (req, res) => {
+// 	res.set();
+// 	res.send(RESPONES.dictionary);
+// });
+
+// app.post('/history', (req, res) => {
+// 	res.send(RESPONES.history);
+// });
+// app.post('/registerV2', (req, res) => {
+// 	res.send(RESPONES.register);
+// });
+// app.post('/auth', (req, res) => {
+// 	res.send({ errorCode: -1 });
+// });
+// app.post('/sendPreview', (req, res) => {
+// 	res.send();
+// });
+// // app.post("/sessionsV2/read", (req, res) => {
+// // res.send(RESPONES.read);
+// // res.end()
+// //     res.set("Connection", "close");
+// // });
+// app.post('/sessionsV2/subscribe', (req, res) => {
+// 	res.send();
+// });
