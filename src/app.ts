c07d9e304059184c91dce796dd85d0311d184efe
Ян Хи Чан <yan.k.ch@sberbank.ru>
2020-01-09 14:28:56 +0300

    update deps

--- src/app.ts
+++ src/app.ts
@@ -1,64 +1,61 @@
-const express = require('express');
-const cors = require('cors');
-const RESPONES = require('../fixtures');
+import express, { Request, Response } from 'express';
+import cors from 'cors';
 import { visits, doctors, clinics, User1 } from './tempData';
-const uuid = require('uuid/v1');
-const fileUpload = require('express-fileupload');
+import uuid from 'uuid/v1';
+import fileUpload from 'express-fileupload';
 const app = express();
 
-import { Request, Response } from 'express';
-
 app.use(
-	cors({
-		credentials: true
-	})
+  cors({
+    credentials: true,
+  })
 );
 app.use(express.urlencoded());
 app.use(express.json());
 app.use(
-	fileUpload({
-		createParentPath: true
-	})
+  fileUpload({
+    createParentPath: true,
+  })
 );
 app.listen(3000, () => console.log('working server!'));
 
 app.get('/', (req: Request, res: Response) => {
-	res.send('express is send!');
+  res.send('express is send!');
 });
 
 app.get('/visits', (req: Request, res: Response) => {
-	res.send(visits);
+  res.send(visits);
 });
 app.post('/newvisit', (req: Request, res: Response) => {
-	console.log(req.body);
-	const newId = uuid();
-	const newVisitWithId = { ...req.body, visitId: newId };
-	res.send(newVisitWithId);
+  console.log(req.body);
+  const newId = uuid();
+  const newVisitWithId = { ...req.body, visitId: newId };
+  res.send(newVisitWithId);
 });
 
 app.get('/doctors', (req: Request, res: Response) => {
-	res.send(doctors);
+  res.send(doctors);
 });
 app.get('/adddoctor', (req: Request, res: Response) => {
-	console.log('adding doc', req.body);
-	res.send(req.body);
+  console.log('adding doc', req.body);
+  res.send(req.body);
 });
 
 app.get('/clinics', (req: Request, res: Response) => {
-	res.send(clinics);
+  res.send(clinics);
 });
 app.get('/addclinic', (req: Request, res: Response) => {
-	console.log('adding clinic', req.body);
-	res.send(req.body);
+  console.log('adding clinic', req.body);
+  res.send(req.body);
 });
 app.post('/addfile', (req: any, res: Response) => {
-	console.log(req.files.file);
-	res.send(req.files.file);
+  console.log(req.files.file);
+  res.send(req.files.file);
 });
 app.get('/userData', (req: any, res: Response) => {
-	res.send(User1);
+  res.send(User1);
 });
 app.post('/adduser', (req: any, res: Response) => {
-	console.log('adding user', req.body)
-	res.send(req.body);
+  console.log('adding user', req.body);
+  res.send(req.body);
 });
--- src/tempData.ts
+++ src/tempData.ts
@@ -1,124 +1,124 @@
 import { IDoctor, IClinic, IUser } from '../../medPlan/src/models';
 
 const dumpAddress = {
-	email: 'some@mail.ru',
-	phone: 1234,
-	address: {
-		country: 'Russia',
-		city: 'Moscow',
-		zipCode: 123456,
-		street: 'Pokrovka 21-14'
-	}
+  email: 'some@mail.ru',
+  phone: 1234,
+  address: {
+    country: 'Russia',
+    city: 'Moscow',
+    zipCode: 123456,
+    street: 'Pokrovka 21-14',
+  },
 };
 const narcologist: IDoctor = {
-	doctorId: 'doc5',
-	firstName: 'Ivan',
-	lastName: 'Sidorov',
-	specialization: ['narcologist'],
-	title: 'MD',
-	gender: 'Male',
-	contacts: dumpAddress
+  doctorId: 'doc5',
+  firstName: 'Ivan',
+  lastName: 'Sidorov',
+  specialization: ['narcologist'],
+  title: 'MD',
+  gender: 'Male',
+  contacts: dumpAddress,
 };
 const neurologist: IDoctor = {
-	doctorId: 'doc4',
-	firstName: 'Maria',
-	lastName: 'Johnson',
-	specialization: ['neurologist'],
-	title: 'MD',
-	gender: 'Female',
-	contacts: dumpAddress
+  doctorId: 'doc4',
+  firstName: 'Maria',
+  lastName: 'Johnson',
+  specialization: ['neurologist'],
+  title: 'MD',
+  gender: 'Female',
+  contacts: dumpAddress,
 };
 const psychiatrist: IDoctor = {
-	doctorId: 'doc3',
-	firstName: 'Sergey',
-	lastName: 'Petrov',
-	specialization: ['psychiatrist'],
-	title: 'MD',
-	gender: 'Male',
-	contacts: dumpAddress
+  doctorId: 'doc3',
+  firstName: 'Sergey',
+  lastName: 'Petrov',
+  specialization: ['psychiatrist'],
+  title: 'MD',
+  gender: 'Male',
+  contacts: dumpAddress,
 };
 const oculist: IDoctor = {
-	doctorId: 'doc2',
-	firstName: 'Sergey',
-	lastName: 'Petrov',
-	specialization: ['oculist'],
-	title: 'MD',
-	gender: 'Male',
-	contacts: dumpAddress
+  doctorId: 'doc2',
+  firstName: 'Sergey',
+  lastName: 'Petrov',
+  specialization: ['oculist'],
+  title: 'MD',
+  gender: 'Male',
+  contacts: dumpAddress,
 };
 
 const doctor: IDoctor = {
-	doctorId: 'doc1',
-	firstName: 'Ivan',
-	lastName: 'Morozov',
-	specialization: ['physician'],
-	title: 'MD',
-	gender: 'Male',
-	contacts: dumpAddress
+  doctorId: 'doc1',
+  firstName: 'Ivan',
+  lastName: 'Morozov',
+  specialization: ['physician'],
+  title: 'MD',
+  gender: 'Male',
+  contacts: dumpAddress,
 };
 
 export const doctors = {
-	doc1: doctor,
-	doc2: oculist,
-	doc3: psychiatrist,
-	doc4: neurologist,
-	doc5: narcologist
+  doc1: doctor,
+  doc2: oculist,
+  doc3: psychiatrist,
+  doc4: neurologist,
+  doc5: narcologist,
 };
 
 const clinic: IClinic = {
-	clinicId: 'clinic1',
-	title: 'Best Clinic in the World',
-	contacts: dumpAddress
+  clinicId: 'clinic1',
+  title: 'Best Clinic in the World',
+  contacts: dumpAddress,
 };
 
 const clinic2: IClinic = {
-	clinicId: 'clinic2',
-	title: 'Memorial clinic',
-	contacts: {
-		...dumpAddress,
-		address: { ...dumpAddress.address, city: 'New York' }
-	}
+  clinicId: 'clinic2',
+  title: 'Memorial clinic',
+  contacts: {
+    ...dumpAddress,
+    address: { ...dumpAddress.address, city: 'New York' },
+  },
 };
 export const clinics = {
-	clinic1: clinic,
-	clinic2: clinic2
+  clinic1: clinic,
+  clinic2,
 };
 export const visits = {
-	visit1: {
-		visitId: 'visit1',
-		title: 'visit to dentist one',
-		patiendId: 'patient1',
-		date: '2020-01-06',
-		doctor: doctor,
-		clinic: clinic,
-		time: '12:00',
-		results: {
-			visitId: 'visit1',
-			diagnosis: 'flu',
-			prescriptions: 'stay home for 3 days'
-		}
-	}
-	// visit2: {
-	// 	visitId: 'visit2',
-	// 	title: 'physician visits',
-	// 	patiendId: 'patient1',
-	// 	doctorId: 'doctor1',
-	// 	date: '5/10/19',
-	// 	time: '12:00',
-	// 	clinicId: 'clinic1',
-	// 	results: {
-	// 		visitId: 'visit1',
-	// 		diagnosis: 'flu',
-	// 		prescriptions: 'stay home for 3 days'
-	// 	}
-	// }
+  visit1: {
+    visitId: 'visit1',
+    title: 'visit to dentist one',
+    patiendId: 'patient1',
+    date: '2020-01-06',
+    doctor,
+    clinic,
+    time: '12:00',
+    results: {
+      visitId: 'visit1',
+      diagnosis: 'flu',
+      prescriptions: 'stay home for 3 days',
+    },
+  },
+  // visit2: {
+  // 	visitId: 'visit2',
+  // 	title: 'physician visits',
+  // 	patiendId: 'patient1',
+  // 	doctorId: 'doctor1',
+  // 	date: '5/10/19',
+  // 	time: '12:00',
+  // 	clinicId: 'clinic1',
+  // 	results: {
+  // 		visitId: 'visit1',
+  // 		diagnosis: 'flu',
+  // 		prescriptions: 'stay home for 3 days'
+  // 	}
+  // }
 };
 export const User1: IUser = {
-	userId: 'user1',
-	firstName: 'Ivan',
-	lastName: 'Petrov',
-	login: 'Petrov1',
-	gender: 'Male',
-	password: '1234',
-	contacts: dumpAddress 
+  userId: 'user1',
+  firstName: 'Ivan',
+  lastName: 'Petrov',
+  login: 'Petrov1',
+  gender: 'Male',
+  password: '1234',
+  contacts: dumpAddress,
 };
